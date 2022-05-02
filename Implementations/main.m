f71eb6e427d56896a536c8fd429eecae373ef4a5
Vinit Awale <awale.vinit@gmail.com>
2022-05-02 16:59:43 +0530

    update 5 files and copy 1 file

--- Implementations/main.m
+++ Implementations/main.m
@@ -12,41 +12,39 @@ addpath('./utils');
 % Get .cdata from the video_movie object
 num_frames = video_info.frameCount;                     % Get the number of frames in the video
 
-video = read_video(video_movie, num_frames);            % Read the video as a matrix
+scale_factor = 0.5;                                     % Scale factor for the video
+video = read_video(video_movie, num_frames, scale_factor);            % Read the video as a matrix
                                                         % video ~ [height x width x channel x frame]
 
 % Noise parameters to the video
-sigma = 30;
+sigma = 20;
 kappa = 15;
 s = 0.2;
 
 % Converting video from unit8 to double
 video = double(video);
-sigma = sigma;
-kappa = kappa;
-
 
 video_noisy = add_video_noise(video, num_frames, sigma, kappa, s);
 
 % % % Show the video frame
 % figure; imshow(uint8(video(:,:,:,1))); title('Original Video Frame 1');
 % figure; imshow(uint8(video_noisy(:,:,:,1))); title('Noisy Video Frame 1');
-% 
+
 % % Write the noisy video to a file using the same video parameters as the original video using VideoWriter
 % write_video(video_noisy, '../Data/bus_cif_noisy', video_info);
 
-K = 5;
+K = 2;
 num_patch_match = 5;
 % num_blocks = num_frames/K;
-num_frames = 10;
-patch_size = 8;
+num_frames = 3;
+patch_size = 16;
 threshold_omega = 50;                               % Threshold used for forming omega
 
 H = size(video,1);                                  % Height of the video
 W = size(video,2);                                  % Width of the video
-
-denoised_video = zeros(H,W,3,num_frames);
-denoised_video_counts = zeros(H,W,3,num_frames);
+C = size(video,3);                                  % No. of channels
+denoised_video = zeros(H,W,C,num_frames);
+denoised_video_counts = zeros(H,W,C,num_frames);
 
 for k = 1:num_frames
     disp(k);
@@ -63,7 +61,9 @@ for k = 1:num_frames
 
     % Get patches from the video
     for i = 1:H - patch_size + 1
+        disp(i);
         for j = 1:W - patch_size + 1
+            disp(j);
             patch = block_video(i:i+patch_size-1, j:j+patch_size-1, :, k);
             [P_jk, P_jk_indices, row_col_indices] = patch_matching_and_grouping(patch, patch_size, num_patch_match, block_video);
 
@@ -89,19 +89,30 @@ write_video(denoised_video, '../Data/bus_cif_denoised', video_info);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%               FUNCTIONS                 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function video = read_video(video_movie, num_frames)
+function video = read_video(video_movie, num_frames, scale_factor)
     % Read the video
     video = [];
     for i = 1:num_frames
-        video = cat(4, video, video_movie(i).cdata);
+        video_frame = video_movie(i).cdata;
+        
+        % Resize the video
+        video_frame = imresize(video_frame, scale_factor);
+
+        video = cat(4, video, video_frame);
     end
 end
 
-function video = read_grayscale_video(video_movie, num_frames)
+function video = read_grayscale_video(video_movie, num_frames, scale_factor)
     % Read the video
     video = [];
     for i = 1:num_frames
-        video = cat(4, video, rgb2gray(video_movie(i).cdata));
+        video_frame = video_movie(i).cdata;
+        video_frame = rgb2gray(video_frame);
+
+        % Resize the video
+        video_frame = imresize(video_frame, scale_factor);
+
+        video = cat(4, video, video_frame);
     end
 end
 
--- Implementations/reconstruct_video.m
+++ Implementations/reconstruct_video.m
@@ -1,44 +1,25 @@
-function [denoised_video, denoised_video_counts] = reconstruct_video(Q_jk, P_jk_indices, denoised_video, denoised_video_counts, patch_size, num_patch_match, size_block, block_start, block_end)
+function [denoised_video, denoised_video_counts] = reconstruct_video(Q_jk, row_col_indices, denoised_video, denoised_video_counts, patch_size, num_patch_match, size_block, block_start, block_end)
 
     H = size_block(1);
-    W = size_block(2)
+    W = size_block(2);
     denoised_block = zeros(size_block);
     denoised_block_counts = zeros(size_block);
     for k = 1:floor(size(Q_jk, 2)/num_patch_match)
-        % Things of interest at current frame
-        Q_jk_k = Q_jk(:, (k-1)*num_patch_match+1:k*num_patch_match);
-        P_jk_indices_k = P_jk_indices(:, (k-1)*num_patch_match+1:k*num_patch_match);
+        patches_considered = row_col_indices(:,:,k);
+        Q_k = Q_jk(:,(k-1)*num_patch_match+1:k*num_patch_match);
+        for j = 1:num_patch_match
+            denoised_block(patches_considered(j,1):patches_considered(j,1)+patch_size-1, patches_considered(j,2):patches_considered(j,2)+patch_size-1, 1, k) = denoised_block(patches_considered(j,1):patches_considered(j,1)+patch_size-1, patches_considered(j,2):patches_considered(j,2)+patch_size-1, 1, k) + reshape(Q_k(1:patch_size^2, j), patch_size, patch_size);
 
-        red_channel = zeros(H,W);
-        green_channel = zeros(H,W);
-        blue_channel = zeros(H,W);
+            denoised_block_counts(patches_considered(j,1):patches_considered(j,1)+patch_size-1, patches_considered(j,2):patches_considered(j,2)+patch_size-1, 1, k) = denoised_block_counts(patches_considered(j,1):patches_considered(j,1)+patch_size-1, patches_considered(j,2):patches_considered(j,2)+patch_size-1, 1, k) + 1;
 
-        red_channel_counts = zeros(H,W);
-        green_channel_counts = zeros(H,W);
-        blue_channel_counts = zeros(H,W);
+            denoised_block(patches_considered(j,1):patches_considered(j,1)+patch_size-1, patches_considered(j,2):patches_considered(j,2)+patch_size-1, 2, k) = denoised_block(patches_considered(j,1):patches_considered(j,1)+patch_size-1, patches_considered(j,2):patches_considered(j,2)+patch_size-1, 2, k) + reshape(Q_k(patch_size^2+1:2*patch_size^2, j), patch_size, patch_size);
 
-        red_channel_cols = im2col(red_channel, [patch_size patch_size], 'sliding');
-        green_channel_cols = im2col(green_channel, [patch_size patch_size], 'sliding');
-        blue_channel_cols = im2col(blue_channel, [patch_size patch_size], 'sliding');
+            denoised_block_counts(patches_considered(j,1):patches_considered(j,1)+patch_size-1, patches_considered(j,2):patches_considered(j,2)+patch_size-1, 2, k) = denoised_block_counts(patches_considered(j,1):patches_considered(j,1)+patch_size-1, patches_considered(j,2):patches_considered(j,2)+patch_size-1, 2, k) + 1;
 
-        red_channel_cols_counts = im2col(red_channel_counts, [patch_size patch_size], 'sliding');
-        green_channel_cols_counts = im2col(green_channel_counts, [patch_size patch_size], 'sliding');
-        blue_channel_cols_counts = im2col(blue_channel_counts, [patch_size patch_size], 'sliding');
+            denoised_block(patches_considered(j,1):patches_considered(j,1)+patch_size-1, patches_considered(j,2):patches_considered(j,2)+patch_size-1, 3, k) = denoised_block(patches_considered(j,1):patches_considered(j,1)+patch_size-1, patches_considered(j,2):patches_considered(j,2)+patch_size-1, 3, k) + reshape(Q_k(2*patch_size^2+1:3*patch_size^2, j), patch_size, patch_size);
 
-        red_channel_cols(:, P_jk_indices_k(1,:)) = Q_jk_k(1:patch_size^2, :);
-        green_channel_cols(:, P_jk_indices_k(1,:)) = Q_jk_k(patch_size^2+1:2*patch_size^2, :);
-        blue_channel_cols(:, P_jk_indices_k(1,:)) = Q_jk_k(2*patch_size^2+1:3*patch_size^2, :);
-
-        red_channel_cols_counts(:, P_jk_indices_k(1,:)) = ones(patch_size^2, num_patch_match);
-        green_channel_cols_counts(:, P_jk_indices_k(1,:)) = ones(patch_size^2, num_patch_match);
-        blue_channel_cols_counts(:, P_jk_indices_k(1,:)) = ones(patch_size^2, num_patch_match);
-
-        red_channel = col2im(red_channel_cols, [patch_size patch_size], [H W], 'sliding');
-        green_channel = col2im(green_channel_cols, [patch_size patch_size], [H W], 'sliding');
-        blue_channel = col2im(blue_channel_cols, [patch_size patch_size], [H W], 'sliding');
-
-        denoised_block(:,:,:,k) = cat(3, red_channel, green_channel, blue_channel);
-        denoised_block_counts(:,:,:,k) = cat(3, red_channel_cols_counts, green_channel_cols_counts, blue_channel_cols_counts);
+            denoised_block_counts(patches_considered(j,1):patches_considered(j,1)+patch_size-1, patches_considered(j,2):patches_considered(j,2)+patch_size-1, 3, k) = denoised_block_counts(patches_considered(j,1):patches_considered(j,1)+patch_size-1, patches_considered(j,2):patches_considered(j,2)+patch_size-1, 3, k) + 1;
+        end
     end
 
     denoised_video(:,:,:,block_start:block_end) = denoised_video(:,:,:,block_start:block_end) + denoised_block;
